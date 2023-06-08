cca7abb171a9e5b2d3a94a90e7474e88fbdc246a
Erick Oduniyi <31193655+eoduniyi@users.noreply.github.com>
2023-06-08 17:39:43 -0400

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,11 +1,8 @@
 let video;
 let webcam;
 let prevWebcamFrame;
-let accumulatedImage;
-
-// New: Bar data array and bar duration
 let bars = [];
-let barDuration = 1000; // Duration in milliseconds
+let barDuration = 3000; // Duration for bars to stay on the screen in milliseconds
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -23,10 +20,6 @@ function setup() {
   // Initialize prevWebcamFrame to store the previous frame
   prevWebcamFrame = createImage(width, height);
   prevWebcamFrame.copy(webcam, 0, 0, width, height, 0, 0, width, height);
-
-  // Initialize accumulatedImage to store the flipped columns
-  accumulatedImage = createImage(width, height);
-  accumulatedImage.copy(video, 0, 0, width, height, 0, 0, width, height);
 }
 
 function videoReady() {
@@ -35,46 +28,21 @@ function videoReady() {
 
 function draw() {
   background(255);
+  video.loadPixels();
+  image(video, 0, 0, width, height); // Always display the current video frame
   pixelVideoWebcamControlled();
-
-  // New: Check bar durations and reset if necessary
-  let currentTime = millis();
-  for (let i = bars.length - 1; i >= 0; i--) {
-    let bar = bars[i];
-    if (currentTime - bar.startTime > barDuration) {
-      // Reset this part of accumulatedImage back to the original video
-      accumulatedImage.copy(
-        video,
-        bar.columnStart,
-        0,
-        bar.columnWidth,
-        height,
-        bar.columnStart,
-        0,
-        bar.columnWidth,
-        height
-      );
-      bars.splice(i, 1); // Remove this bar from the array
-    }
-  }
 }
 
 function pixelVideoWebcamControlled() {
   let movementThreshold = 100;
   let columnWidth = video.width / 50; // Width of each column
 
-  // Mirror the webcam feed horizontally
   webcam.loadPixels();
   prevWebcamFrame.loadPixels();
 
-  // Display the non-pixelated video
-  image(video, 0, 0, width, height);
-
-  video.loadPixels();
-  accumulatedImage.loadPixels();
   push();
   translate(width, 0);
-  scale(-1, 1);
+  scale(-1, 1); // Flip webcam feed
   for (let y = 0; y < webcam.height; y++) {
     for (let x = 0; x < webcam.width; x++) {
       let index = (y * webcam.width + x) * 4;
@@ -86,20 +54,20 @@ function pixelVideoWebcamControlled() {
       if (diff > movementThreshold) {
         let columnStart = floor(x / columnWidth) * columnWidth;
 
-        // Copy a flipped column of the video frame to accumulatedImage
-        accumulatedImage.copy(
+        // Copy a flipped column of the video frame
+        image(
           video,
-          columnStart + columnWidth,
-          0,
-          -columnWidth,
-          video.height,
           columnStart,
           0,
           columnWidth,
+          video.height,
+          columnStart + columnWidth,
+          0,
+          -columnWidth,
           video.height
         );
 
-        // New: Add this bar to the array with the current time and position
+        // Add this bar to the array with the current time and position
         bars.push({
           startTime: millis(),
           columnStart: columnStart,
@@ -112,9 +80,24 @@ function pixelVideoWebcamControlled() {
   }
   pop();
 
-  // Display the accumulatedImage on top of the video
-  image(accumulatedImage, 0, 0, width, height);
-
   // Update prevWebcamFrame with the current frame
   prevWebcamFrame.copy(webcam, 0, 0, width, height, 0, 0, width, height);
+
+  // Remove bars that have been on the screen for more than barDuration milliseconds
+  bars = bars.filter((bar) => millis() - bar.startTime < barDuration);
+
+  // Display the remaining bars
+  bars.forEach((bar) => {
+    image(
+      video,
+      bar.columnStart,
+      0,
+      bar.columnWidth,
+      video.height,
+      bar.columnStart + bar.columnWidth,
+      0,
+      -bar.columnWidth,
+      video.height
+    );
+  });
 }
